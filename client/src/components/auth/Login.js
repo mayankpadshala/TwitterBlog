3b2fefd141aeeff8d82d09a73998411c1bc98370
JahnaviSalammagari <jahnavireddy.salammagari@gmail.com>
2023-11-29 12:51:36 -0800

    login implementation

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -4,68 +4,113 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { login } from '../../actions/auth';
 
-const Login = ({ login, isAuthenticated }) => {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: ''
-  });
+// const Login = ({ login, isAuthenticated }) => {
+//   const [formData, setFormData] = useState({
+//     email: '',
+//     password: ''
+//   });
 
-  const { email, password } = formData;
+//   const { email, password } = formData;
 
-  const onChange = (e) =>
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+//   const onChange = (e) =>
+//     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    login(email, password);
-  };
+//   const onSubmit = (e) => {
+//     e.preventDefault();
+//     login(email, password);
+//   };
 
-  if (isAuthenticated) {
-    return <Navigate to="/posts" />;
-  }
+//   if (isAuthenticated) {
+//     return <Navigate to="/posts" />;
+//   }
 
-  return (
-    <section className="container">
-      <h1 className="large text-primary">Sign In</h1>
-      <p className="lead">
-        <i className="fas fa-user" /> Sign Into Your Account
-      </p>
-      <form className="form" onSubmit={onSubmit}>
-        <div className="form-group">
-          <input
-            type="email"
-            placeholder="Email Address"
-            name="email"
-            value={email}
-            onChange={onChange}
-          />
+//   return (
+//     <section className="container">
+//       <h1 className="large text-primary">Sign In</h1>
+//       <p className="lead">
+//         <i className="fas fa-user" /> Sign Into Your Account
+//       </p>
+//       <form className="form" onSubmit={onSubmit}>
+//         <div className="form-group">
+//           <input
+//             type="email"
+//             placeholder="Email Address"
+//             name="email"
+//             value={email}
+//             onChange={onChange}
+//           />
+//         </div>
+//         <div className="form-group">
+//           <input
+//             type="password"
+//             placeholder="Password"
+//             name="password"
+//             value={password}
+//             onChange={onChange}
+//             minLength="6"
+//           />
+//         </div>
+//         <input type="submit" className="btn btn-primary" value="Login" />
+//       </form>
+//       <p className="my-1">
+//         Don't have an account? <Link to="/register">Sign Up</Link>
+//       </p>
+//     </section>
+//   );
+// };
+
+// Login.propTypes = {
+//   login: PropTypes.func.isRequired,
+//   isAuthenticated: PropTypes.bool
+// };
+
+// const mapStateToProps = (state) => ({
+//   isAuthenticated: state.auth.isAuthenticated
+// });
+
+// export default connect(mapStateToProps, { login })(Login);
+
+
+import googleImage from '../../assets/googleImage.png';
+import githubImage from '../../assets/githubImage.png';
+import twitterImage from '../../assets/twitterImage.png';
+
+export default function LoginPage() {
+
+    const googleLogin = () => {
+        window.open("http://localhost:5000/auth/google", "_self");
+    }
+
+    const githubLogin = () => {
+        window.open("http://localhost:5000/auth/github", "_self");
+    }
+    
+    const twitterLogin = () => {
+        window.location.href = "http://localhost:5000/auth/twitter"
+    }
+
+
+    return (
+        <div className="loginPage">
+            <div className="loginForm">
+                <h1>Login</h1>
+                <div className="googleContainer" onClick={googleLogin}>
+                    <img src={googleImage} alt="Google Icon" />
+                    <p>Login With Google</p>
+                </div>
+
+                <div className=".googleContainer .githubContainer" onClick={githubLogin}>
+                    <img src={githubImage} alt="Github Icon" />
+                    <p>Login With Github</p>
+                </div>
+
+                <div className=".googleContainer .twitterContainer" onClick={twitterLogin}>
+                    <img src={twitterImage} alt="Twitter Icon" />
+                    <p>Login With Twitter</p>
+                </div>
+
+            </div>
+
         </div>
-        <div className="form-group">
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={password}
-            onChange={onChange}
-            minLength="6"
-          />
-        </div>
-        <input type="submit" className="btn btn-primary" value="Login" />
-      </form>
-      <p className="my-1">
-        Don't have an account? <Link to="/register">Sign Up</Link>
-      </p>
-    </section>
-  );
-};
-
-Login.propTypes = {
-  login: PropTypes.func.isRequired,
-  isAuthenticated: PropTypes.bool
-};
-
-const mapStateToProps = (state) => ({
-  isAuthenticated: state.auth.isAuthenticated
-});
-
-export default connect(mapStateToProps, { login })(Login);
+    )
+}
--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger');
 //const redis = require('redis');
 const { logger, morganMiddleware } = require('./logger');
-
+const session = ('express-session');
+const passport =require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const TwitterStrategy = require('passport-twitter').Strategy;
+const GitHubStrategy = require('passport-github').Strategy;
+const cors = require('cors');
+const User = require('./models/User');
 const app = express();
 
 // Connect Database
@@ -24,13 +30,31 @@ connectDB();
 
 
 // Init Middleware
+// app.use(express.json());
+// app.use('/uploads', express.static(__dirname + '/uploads'));
+// app.use(cookieParser());
+
 app.use(express.json());
-app.use('/uploads', express.static(__dirname + '/uploads'));
-app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5000", credentials: true }))
+
+app.set("trust proxy", 1);
+
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      sameSite: "none",
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7 // One Week
+    }
+  }))
+
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
+//app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/messages', require('./routes/api/messages'));
@@ -38,6 +62,149 @@ app.use('/api/upload', require('./routes/api/upload'));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
 app.use(morganMiddleware);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.serializeUser((user, done) => {
+  return done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+
+  User.findById(id, (err, doc) => {
+    // Whatever we return goes to the client and binds to the req.user property
+    return done(null, doc);
+  })
+})
+
+
+passport.use(new GoogleStrategy({
+  clientID: "602322387096-ds7lr4lnm637uamuu56m8hflcmbc3gfh.apps.googleusercontent.com",
+  clientSecret: "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",
+  callbackURL: "/auth/google/callback"
+},
+  function (_ , __, profile, cb) {
+    console.log("Passport google backend")
+    User.findOne({ googleId: profile.id }, async (err, doc) => {
+
+      if (err) {
+        return cb(err, null);
+      }
+
+      if (!doc) {
+        const newUser = new User({
+          googleId: profile.id,
+          username: profile.name.givenName
+        });
+
+        await newUser.save();
+        cb(null, newUser);
+      }
+      cb(null, doc);
+    })
+
+  }));
+
+passport.use(new TwitterStrategy({
+  consumerKey: `${process.env.TWITTER_CLIENT_ID}`,
+  consumerSecret: `${process.env.TWITTER_CLIENT_SECRET}`,
+  callbackURL: "/auth/twitter/callback"
+},
+  function (_ , __ , profile, cb) {
+
+    User.findOne({ twitterId: profile.id }, async (err, doc) => {
+
+      if (err) {
+        return cb(err, null);
+      }
+
+      if (!doc) {
+        const newUser = new User({
+          twitterId: profile.id,
+          username: profile.username
+        });
+
+        await newUser.save();
+        cb(null, newUser);
+      }
+      cb(null, doc);
+    })
+
+  }
+));
+
+passport.use(new GitHubStrategy({
+  clientID: `${process.env.GITHUB_CLIENT_ID}`,
+  clientSecret: `${process.env.GITHUB_CLIENT_SECRET}`,
+  callbackURL: "/auth/github/callback"
+},
+  function (_, __, profile, cb) {
+
+    User.findOne({ githubId: profile.id }, async (err, doc) => {
+
+      if (err) {
+        return cb(err, null);
+      }
+
+      if (!doc) {
+        const newUser = new User({
+          githubId: profile.id,
+          username: profile.username
+        });
+
+        await newUser.save();
+        cb(null, newUser);
+      }
+      cb(null, doc);
+    })
+
+  }
+));
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: 'https://gallant-hodgkin-fb9c52.netlify.app', session: true }),
+  function (req, res) {
+    res.redirect('http://localhost:5000/posts');
+  });
+
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: 'https://gallant-hodgkin-fb9c52.netlify.app', session: true }),
+  function (req, res) {
+    res.redirect('https://gallant-hodgkin-fb9c52.netlify.app');
+  });
+
+
+app.get('/auth/github', passport.authenticate('github'));
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: 'https://gallant-hodgkin-fb9c52.netlify.app', session: true }),
+  function (req, res) {
+    res.redirect('https://gallant-hodgkin-fb9c52.netlify.app');
+  });
+
+
+
+app.get("/", (req, res) => {
+  res.send("Helllo WOlrd");
+})
+
+app.get("/getuser", (req, res) => {
+  res.send(req.user);
+})
+
+app.get("/auth/logout", (req, res) => {
+  if (req.user) {
+    req.logout();
+    res.send("done");
+  }
+})
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
